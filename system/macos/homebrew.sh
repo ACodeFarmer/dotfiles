c3b5124cd9fc9b1b8464fd4c4d2416facbb99d3e
Pumsuk Cho <codefarmer@Mac-mini.local>
2017-10-09 22:29:55 +0900

    OS 별 설치 스크립트 구조 변경 및 Brewfile 정리

--- system/macos/homebrew.sh
+++ system/macos/homebrew.sh
@@ -16,10 +16,32 @@ then
   ask_for_confirmation "Homebrew Bundle 을 설치 할까요?"
   if answer_is_yes; then
     brew tap Homebrew/bundle
-    #brew bundle
-    #brew bundle --file=devel/vagrant.Brewfile
-  fi  
+  fi
 
+  ask_for_confirmation "Homebrew 를 통해 기본 애플리케이션들을 설치 할까요?"
+  if answer_is_yes; then
+    brew bundle --file=$DOTFILES_ROOT/system/macos/Brewfile
+  fi
+  
+  ask_for_confirmation "Homebrew 를 통해 기본 개발 패키지들을 설치 할까요?"
+  if answer_is_yes; then
+    brew bundle --file=$DOTFILES_ROOT/system/macos/devel/Brewfile
+  fi
+  
+  ask_for_confirmation "Homebrew 를 통해 NodeJS 개발 패키지들을 설치 할까요?"
+  if answer_is_yes; then
+    brew bundle --file=$DOTFILES_ROOT/system/macos/devel/node.Brewfile
+  fi
+  
+  ask_for_confirmation "Homebrew 를 통해 Ruby 개발 패키지들을 설치 할까요?"
+  if answer_is_yes; then
+    brew bundle --file=$DOTFILES_ROOT/system/macos/devel/ruby.Brewfile
+  fi
+  
+  ask_for_confirmation "Homebrew 를 통해 Vagrant 패키지들을 설치 할까요?"
+  if answer_is_yes; then
+    brew bundle --file=$DOTFILES_ROOT/system/macos/devel/vagrant.Brewfile
+  fi
 else
   print_message '이 섹션은 OSX를 위해서만 작성되었습니다. 리눅스용 Homebrew 사용은 지원하지 않습니다.'
 fi
